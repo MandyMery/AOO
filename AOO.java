ba9d5cce9d4043157999f3a46d80e81d860d902a
merimaha <merima@club.fr>
2019-12-13 11:53:20 +0100

    initial commit

---
+++ AOO.java
@@ -0,0 +1,19 @@
+/*
+Modéliser la structure de Age of Empires 2
+objets:
+univers
+cartes
+civilisation
+tribus
+unités
+armes
+-uniquement les classes, interfaces.
+-pas de logique de jeu(combats etc)
+-travail par groupe de 2 sur GITHUB
+-README.TXT documente la structure des classes.
+*/
+
+class AOO
+{
+
+}
